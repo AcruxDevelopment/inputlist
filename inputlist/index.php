af06f71ad62c44b47ed2890f7b02bddcee63ec3a
AcruxDevelopment <acruxstudios.dev@gmail.com>
2025-10-15 14:54:45 -0300

    fixed styles

--- inputlist/index.php
+++ inputlist/index.php
@@ -9,7 +9,7 @@
 </head>
 <body>
     <h2>HTML Dynamic Input List TEST</h2>
-    <div id="ilist-1" ilist-suplier="IListMateriasSuplier" ilist-title="Materias" ilist-inames="materias[]"></div>
+    <div id="ilist-1" ilist-suplier="IListPhoneNumberSuplier" ilist-title="Materias" ilist-inames="materias[]"></div>
     <script>
         generateIList(document.getElementById("ilist-1"));
     </script>
